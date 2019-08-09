5378eb589cb0ed5006da61cf8b33f3ff0c6a491b
alxsrobert <alexis3.robert3@gmail.com>
2019-08-09 13:18:25 +0100

    add priors.h and moves.h

---
+++ src/priors.h
@@ -0,0 +1,20 @@
+#ifndef OUTBREAKER2_PRIORS_H
+#define OUTBREAKER2_PRIORS_H
+
+
+
+double cpp_prior_pi(Rcpp::List param, Rcpp::List config,
+                    Rcpp::RObject custom_function);
+
+double cpp_prior_a(Rcpp::List param, Rcpp::List config,
+                   Rcpp::RObject custom_function);
+
+double cpp_prior_b(Rcpp::List param, Rcpp::List config,
+                   Rcpp::RObject custom_function);
+
+double cpp_prior_all(Rcpp::List param, Rcpp::List config,
+                     Rcpp::RObject custom_functions = R_NilValue);
+
+
+#endif
+
