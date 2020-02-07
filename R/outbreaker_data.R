8867446f7c0375c00994cba5e13aeef129165626
alxsrobert <alexis3.robert3@gmail.com>
2020-02-07 18:26:01 +0000

    Fix typo

--- R/outbreaker_data.R
+++ R/outbreaker_data.R
@@ -312,7 +312,7 @@ outbreaker_data <- function(..., data = list(...)) {
       if(!is.null(data$w_dens) & !is.null(data$dates)){
         unlik_w_dens <- which(data$log_w_dens[-1] < -40 &
                                 diff(data$log_w_dens[1,]) < 0)[1]
-        can_be_ances_X[data$dates[X] - unlik_w_dens < data$dates] <- FALSE
+        can_be_ances_X[data$dates[X] - unlik_w_dens > data$dates] <- FALSE
       }
       if(data$genotype[X] != "Not attributed"){
         can_be_ances_X[data$genotype != data$genotype[X] &
