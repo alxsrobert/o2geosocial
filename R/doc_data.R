ae36df5bd2ff159c65cfc63267054f925e134fcf
alxsrobert <alexis3.robert3@gmail.com>
2020-06-30 18:17:05 +0100

    Update data description

--- R/doc_data.R
+++ R/doc_data.R
@@ -1,14 +1,14 @@
 #' Simulated outbreaks
 #'
 #' We generated two datasets used to illustrate \code{o2geosocial}. 
-#' The first one (toy_outbreak_long) contains 1940 cases, from simulated outbreaks 
+#' The first one (toy_outbreak_long) contains 1,940 cases, from simulated outbreaks 
 #' nationwide between 2010 and 2017. The list contains the following:
 #'
 #' \itemize{
 #'
 #' \item \code{$cases}: A data table summarising the epidemiological features of the
-#' 1000 cases. It contains the State, onset date,  genotype,  county, age group,
-#' import status, cluster and generation of the cases.
+#' 1,940 cases. It contains the ID, State, onset date,  genotype,  county, age group,
+#' import status, cluster, generation and infector of the cases.
 #'
 #' \item \code{$dt_regions}: A data table containing the ID, population, longitude 
 #' and latitude of each region. Should be used to compute the distance matrix, using 
@@ -34,7 +34,7 @@
 #' Simulated outbreaks
 #'
 #' Second dataset used to illustrate \code{o2geosocial}. (toy_outbreak_short) is a 
-#' smaller data set (350 cases), spread across different Census tracks in Ohio 
+#' smaller data set (75 cases), spread across different Census tracks in Ohio 
 #' (population and location of each region taken from 
 #' https://www.census.gov/geographies/reference-files/2010/geo/2010-centers-population.html).
 #' The list contains the following:
@@ -42,8 +42,8 @@
 #' \itemize{
 #'
 #' \item \code{$cases}: A data table summarising the epidemiological features of the
-#' 1000 cases. It contains the State, onset date,  genotype,  county, age group,
-#' import status, cluster and generation of the cases.
+#' 75 cases. It contains the ID, state, onset date,  genotype, Census tract, age group,
+#' import status, cluster, generation and infector of the cases.
 #'
 #' \item \code{$dt_regions}: A data table containing the ID, population, longitude 
 #' and latitude of each region. Should be used to compute the distance matrix, using 
