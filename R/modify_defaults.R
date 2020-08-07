d992bb5e1b7a8d1eb02d65708ab4ce3a3d5d59df
alxsrobert <alexis3.robert3@gmail.com>
2020-08-07 17:53:20 +0100

    Update description of the function

--- R/modify_defaults.R
+++ R/modify_defaults.R
@@ -1,6 +1,7 @@
-## This function was contributed by Rich Fitzjohn. It modifies default arguments
-## using user-provided values. The argument 'strict' triggers and error
-## behaviour: if strict==TRUE: all new values need to be part of the defaults.
+## This function was contributed by Rich Fitzjohn in outbreaker2. 
+## It modifies default arguments using user-provided values. 
+## The argument 'strict' triggers and error behaviour: 
+## if strict==TRUE: all new values need to be part of the defaults.
 
 modify_defaults <- function (defaults, x, strict = TRUE) {
   extra <- setdiff(names(x), names(defaults))
